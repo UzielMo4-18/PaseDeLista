f5010bf485fec24238b67a17839a7f9bd8497d29
UzielM <uzielmoreno18@gmail.com>
2020-12-02 23:02:11 -0600

    Adicion de modelos y migracion de la BD

--- PaseLista_Project/home/admin.py
+++ PaseLista_Project/home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Alumno,Profesor,Materia,Clase,Asistencia
 
 # Register your models here.
+admin.site.register(Alumno)
+admin.site.register(Profesor)
+admin.site.register(Materia)
+admin.site.register(Clase)
+admin.site.register(Asistencia)
