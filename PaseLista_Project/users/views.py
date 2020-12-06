2d0b990a8b66d78c47a045429a3d74537d3b2383
UzielM <uzielmoreno18@gmail.com>
2020-12-06 15:22:59 -0600

    Ajustes a los templates y vistas

--- PaseLista_Project/users/views.py
+++ PaseLista_Project/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-from home.models import Alumno,Profesor
+from home.models import Alumno,Profesor,Materia
 
 # Create your views here.
 
@@ -8,5 +8,9 @@ class UsersSc(View):
     def get(self,request):
         alumnos=Alumno.objects.all()
         profesores=Profesor.objects.all()
-        contexto={'alumnos':alumnos,'profesores':profesores}
+        materias_profesor=Materia.objects.all()
+        auxpr=[]
+        for pr in profesores: auxpr.append(pr.id)
+        print(auxpr)
+        contexto={'alumnos':alumnos,'profesores':profesores,'materias_profesor':materias_profesor,'auxpr':auxpr}
         return render(request,'users.html',contexto)
